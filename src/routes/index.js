2433fb60c016e44f4e151c73543a228cd51b1906
Pedrox20/ PT-05/ GroupN°6 <92409397+Pedrox20@users.noreply.github.com>
2022-11-29 23:18:21 -0300

    creacion de boilerplate

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+const axios= require("axios");
+
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+const router = Router();
+
+
+module.exports = router;
