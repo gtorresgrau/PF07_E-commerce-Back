a6de3a63a6f796300120ee19fa70076f9fc2a30a
gtorresgrau <gonzalotorresgrau@gmail.com>
2022-12-23 12:33:42 -0300

    filters

--- src/routes/filterAnidado.js
+++ src/routes/filterAnidado.js
@@ -120,4 +120,4 @@ router.get('/', async ( req,res)=>{
     }
 })
 
-module.exports= router;
+module.exports = router;
--- src/routes/filterBrand.js
+++ src/routes/filterBrand.js
@@ -1,14 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 const {brandFilter}=require('../controllers/filterBrand')
-    
+ 
 router.get('/:brand', async ( req,res)=>{
     const {brand} = req.params;
     try {
         const search = await brandFilter(brand);
         res.status(200).send(search)
-    }
-     catch (error) { 
+    }catch (error) { 
         res.status(404).send("Brand Not found")
     }
 });
