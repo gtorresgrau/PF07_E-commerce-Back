15180f858bc26715ee3ad7464553038e276d97b2
gtorresgrau <gonzalotorresgrau@gmail.com>
2023-01-04 13:22:05 -0300

    getReview

--- src/controllers/getReviews.js
+++ src/controllers/getReviews.js
@@ -1,17 +1,21 @@
 const { Review } = require("../db");
 
 const reviewId = async (id) => {
-  console.log('BaReviewID_id:',id)
+  console.log('Ba_ID_id:',id)
   const reviewId = await Review.findAll({where: {sneakerId: id}});
   console.log('BaReviewId:',reviewId)
-
-  const sneakerID = {
-    stars:reviewId.stars,
-    text:reviewId.text,
-    sneakerId:reviewId.id,
-    userId:reviewId.userId
-};
-  console.log('BaSneakerID:',sneakerID);
-  return sneakerID;
+    if(reviewId.length){
+      const sneakerID = {
+        stars:reviewId.stars,
+        text:reviewId.text,
+        sneakerId:reviewId.id,
+        userId:reviewId.userId
+      };
+    }else{
+      reviewId = 'There are no reviews for this sneakers';
+    }
+    console.log('BaSneakerID:',sneakerID);
+    return sneakerID;
+    
 };
 module.exports = { reviewId };
--- src/routes/getReviews.js
+++ src/routes/getReviews.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-const {reviewId} = require('../controllers/getSneaker')
+const {reviewId} = require('../controllers/getReviews')
 
 router.get('/:id', async ( req,res)=>{
     const {id} = req.params;
@@ -8,7 +8,7 @@ router.get('/:id', async ( req,res)=>{
         const search = await reviewId(id);
         res.status(200).send(search);
     }catch (error) { 
-            res.status(404).send("There are no reviews for this sneakers")
+            res.status(404).send(`Something happend filtering by ID: ${id}`)
         }
 });
 
