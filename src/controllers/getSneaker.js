8a153bfc5514562ed2693cc46cad8943be6d1a83
gtorresgrau <gonzalotorresgrau@gmail.com>
2023-01-08 17:41:07 -0300

    funcionandoMp

--- src/controllers/getSneaker.js
+++ src/controllers/getSneaker.js
@@ -1,19 +1,26 @@
-const { Sneakers, Review } = require("../db");
+const { Sneakers } = require("../db");
+
 
 
 const sneakerId = async (id) => {
-  const sneakerId = await Sneakers.findOne({
-    where: {
-      id: id
-    },
-    include:{
-      model:Review,
-      through:{
-        attributes:[],
-      }
-    }
-  });
+  const sneakerId = await Sneakers.findOne({where: {id: id}});
 
-  return sneakerId;
+  const sneakerID = {
+    id: sneakerId.id,
+    title: sneakerId.title,
+    price: sneakerId.price,
+    image: sneakerId.image,
+    description: sneakerId.description,
+    size: sneakerId.size,
+    stock: sneakerId.stock,
+    brand:sneakerId.brand,
+    model:sneakerId.model,
+    genre:sneakerId.genre,
+    colour:sneakerId.colour,
+    type:sneakerId.type,
+    rating:sneakerId.rating
+};
+
+  return sneakerID;
 };
 module.exports = { sneakerId };
