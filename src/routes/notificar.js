8c023aa9126f2974665c15372ae1a8f66781b6a0
matirozas <98505362+matirozas@users.noreply.github.com>
2023-01-13 03:12:28 -0300

    Update notificar.js

--- src/routes/notificar.js
+++ src/routes/notificar.js
@@ -19,18 +19,17 @@ router.post('/',async(req,res)=>{
    if(query.topic==='merchant_order'){
       var pay_merc = await mercadopago.merchant_orders.findById(query.id) 
        //console.log('query.id : ',query.id)
-       console.log('notificarMerc: ',pay_merc)
-       console.log('Merc_status:',pay_merc.body);
+       console.log('preference: ',pay_merc.body.preference_id)
+       
        //const payId = query.id
        const statusMer = pay_merc.body.status
-       console.log('statusMer:',statusMer)
+      
       // if(status === 'approved') sendEmail(query.email);
     }
     if(query.topic==='payment'){
         var pay_payment = await mercadopago.payment.findById(query.id) 
          //console.log('query.id : ',query.id)
-         console.log('notificarPay: ',pay_payment)
-         console.log('Payment_status:',pay_payment.body);
+         
          //const payId = query.id
          const statusPay = pay_payment.body.status
          console.log('statusPay:',statusPay)
